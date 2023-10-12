e1fad8424e7bf25ee981a1bf0a3fbd6d2c6e4116
tejas <tejasnasre120@gmail.com>
2023-10-12 12:42:37 +0530

    Adding Basic Program

---
+++ Day-01/basicPy.py
@@ -0,0 +1,20 @@
+#print hello wolrd in Py - Our First Py Program
+
+print('hello wolrd')
+
+# variable In Py 
+
+a= 5
+print(a)
+
+#Take Input frpm User
+
+enterNum = input('enter num: ')
+print(enterNum)
+
+# Adding Two Numbers
+
+num1 = int(input('enter num1: '))
+num2 = int(input('enter num2: '))
+sum = num1 + num2
+print(sum)
