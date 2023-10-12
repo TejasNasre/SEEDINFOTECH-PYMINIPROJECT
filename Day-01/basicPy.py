6c4bdbc02c1aa155649db74ad3f319aa5337fe03
tejas <tejasnasre120@gmail.com>
2023-10-12 13:02:32 +0530

    Added Till Logical

--- Day-01/basicPy.py
+++ Day-01/basicPy.py
@@ -18,3 +18,37 @@ num1 = int(input('enter num1: '))
 num2 = int(input('enter num2: '))
 sum = num1 + num2
 print(sum)
+
+# Addition of Two Float Number
+
+fNum1 = float(input('enter num1: '))
+fNum2 = float(input('enter num2: '))
+sum = fNum1 + fNum2
+print(sum)
+
+# Oprators In Python
+
+# Arithmetic Operators
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 % num2)
+print(num1 // num2)
+
+# Comparison  Operators
+
+print(num1 = num2)
+print(num1 > num2)
+print(num1 < num2)
+print(num1 >= num2)
+print(num1 <= num2)
+print(num1 != num2num1 !)
+
+# Logical Operators
+
+print(num1 > num2 and num1 > num2)
+print(num1 > num2 or num1 > num2)
+print(not num1 > num2)
+
