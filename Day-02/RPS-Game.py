0b19f3e7788d6ff49132bfd01acc86b426a3b591
Soham Khedkar <sohamunderscore99@gmail.com>
2023-10-13 12:25:30 +0530

    Made changes for indentation error

--- Day-02/RPS-Game.py
+++ Day-02/RPS-Game.py
@@ -5,32 +5,31 @@ print('Winning rules of the game ROCK PAPER SCISSORS are:\n'
       '3. Paper vs Scissors -> Scissors wins \n')
 while True :
   print("Enter your choice \n 1-Rock \n 2-Paper \n 3-Scissors")
-  
-choice = int(input("Enter your choice: "))
-while choice >3 or choice <1:
-  chioce = int(input('Enter a calid choice please'))
+  choice = int(input("Enter your choice: "))
+  while choice >3 or choice <1:
+    choice = int(input('Enter a valid choice please'))
   if choice == 1:
     choice_name ='Rock'
   elif choice == 2:
     choice_name = 'Paper'
   else:
     choice_name ='Scissors'
-comp_choice = random.randint(a:1, b:3)
+    comp_choice = random.randint(a:1, b:3)
 
-while comp_choice == choice:
-  comp_choice = random.randint(a:1, b:3)
-if comp_choice == 1:
-  comp_choice_name ='rock'
-elif comp_choice == 2:
-  comp_choice_name ='paper'
-else:
-  comp_choice_name = 'scissors'
-print("Computer choice is \n", comp_choice_name)
-print(choice_name, 'Vs', comp_choice_name)
+  while comp_choice == choice:
+    comp_choice = random.randint(a:1, b:3)
+  if comp_choice == 1:
+   comp_choice_name ='rock'
+  elif comp_choice == 2:
+   comp_choice_name ='paper'
+  else:
+   comp_choice_name = 'scissors'
+  print("Computer choice is \n", comp_choice_name)
+  print(choice_name, 'Vs', comp_choice_name)
 
-if choice == comp_choice:
-  print("It's a draw", end ="")
-  result = "DRAW"
+  if choice == comp_choice:
+   print("It's a draw", end ="")
+   result = "DRAW"
 
 
 if(choice ==1 and comp_choice == 2):
