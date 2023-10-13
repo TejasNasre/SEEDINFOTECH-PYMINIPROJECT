f19f3d0cf357809cf5e0a2a2c7efc11e6fb1e60c
tejas <tejasnasre120@gmail.com>
2023-10-13 13:12:08 +0530

    code added

--- Day-02/intermediate.py
+++ Day-02/intermediate.py
@@ -4,3 +4,12 @@ def demo():
   print('hello world')
 
 demo()
+
+#Odd Even
+def odd_even(num):
+  if num % 2 == 0:
+    print("Even")
+  else:
+    print("Odd")
+
+odd_even(int(input("Enter a number: ")))
